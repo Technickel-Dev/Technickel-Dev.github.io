15e911313bde6874c0f2c65ac020e2eaa8388b4b
Technickel-Dev <bradlabrador@hotmail.com>
2022-01-09 17:34:27 -0500

    Add logo and change favicon

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -3,17 +3,12 @@
 </script>
 
 <nav class="flex items-center justify-between flex-wrap bg-sky-500 p-4 shadow-lg shadow-black">
-  <a href="/" class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg
-      class="fill-current h-8 w-8 mr-2"
-      width="54"
-      height="54"
-      viewBox="0 0 54 54"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-      /></svg
-    >
+  <a href="/" class="flex items-center flex-shrink-0 mr-6">
+    <img
+      class="h-8 w-8 mr-2"
+      src="technickel_logo.svg"
+      alt="The Technickel logo, a 5 cent Canadian coin (nickel)"
+    />
     <span class="font-semibold text-xl tracking-tight">TECHNICKEL</span>
   </a>
   <div class="block lg:hidden">
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <Nav />
-<div class="my-6 mx-16">
+<div class="my-4 mx-16">
   <slot />
 </div>
