3427b71135c58c05f160629618ed3b9f20c1e197
Technickel-Dev <bradlabrador@hotmail.com>
2022-01-11 19:17:19 -0500

    Github actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+  extensions: [".svelte", ...mdsvexConfig.extensions],
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte"
+  },
 
-	preprocess: [preprocess({}), mdsvex(mdsvexConfig)]
+  preprocess: [preprocess({}), mdsvex(mdsvexConfig)]
 };
 
 export default config;
