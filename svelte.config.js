0feba0945559aab14b430f22a79d7f661ac29d79
Technickel-Dev <bradlabrador@hotmail.com>
2022-07-11 01:48:12 -0400

    Remove mdsvex, upgrade svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
+  extensions: [".svelte"],
 
   kit: {
-    adapter: adapter(),
-  },
-
-  preprocess: [preprocess({}), mdsvex(mdsvexConfig)]
+    adapter: adapter()
+  }
 };
 
 export default config;
