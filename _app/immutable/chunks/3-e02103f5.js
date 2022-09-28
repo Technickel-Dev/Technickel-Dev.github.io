7dcc964fd9f8ba691412da76b383d6f3efe770ee
Technickel-Dev <bradlabrador@hotmail.com>
2022-09-28 00:14:26 +0000

    Deploying to gh-pages from @ Technickel-Dev/Technickel-Dev.github.io@820db7e519894da6a7f58d50216ad49e7e70499c 🚀

---
+++ _app/immutable/chunks/3-e02103f5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/contact/_page.svelte-904b027a.js";export{t as component};
---
+++ _app/immutable/chunks/4-610b3d07.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/projects/_page.svelte-6d95b8c6.js";export{t as component};
