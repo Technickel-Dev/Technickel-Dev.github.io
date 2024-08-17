6a1c7d355c6d5c703e945cd0727aa4a76e60cfa8
Bradley Leonard <bradley.leonard@technickel.dev>
2024-08-17 01:18:41 -0400

    Typescript fixing

--- src/routes/(trading)/inventory/[user]/+page.svelte
+++ src/routes/(trading)/inventory/[user]/+page.svelte
@@ -5,30 +5,23 @@
   /** @type {import('./$types').PageData} */
   export let data;
 
-  interface Tag {
-    category: string;
-    internal_name: string;
-    localized_category_name: string;
-    localized_tag_name: string;
-  }
-
   let searchQuery = "";
 
-  function getClassidCounts(assets) {
+  const getClassidCounts = (assets: Asset[]) => {
     if (!assets) return {}; // Return an empty object if assets is null or undefined
 
-    return assets.reduce((acc, asset) => {
+    return assets.reduce<Record<string, number>>((acc, asset) => {
       const { classid } = asset;
       acc[classid] = (acc[classid] || 0) + 1;
       return acc;
     }, {});
-  }
+  };
 
   // Function to get the name and type based on classid
-  function getNameAndTypeByClassid(classid, descriptions) {
-    const description = descriptions.find((desc) => desc.classid === classid);
-    return description ? description : { name: "Unknown", type: "Unknown" };
-  }
+  const getNameAndTypeByClassid = (classid: string, descriptions: Description[]) => {
+    const description = descriptions.find((description) => description.classid === classid);
+    return description ? description : null;
+  };
   // Reactive variable that calls the function
   $: classidCounts = getClassidCounts(data.assets);
 
@@ -37,20 +30,26 @@
       const description = getNameAndTypeByClassid(classid, data.descriptions);
       return { count, description, classid };
     })
-    .filter((card) =>
-      card.description.tags.some((tag: Tag) => tag.localized_tag_name === "Trading Card")
-    );
+    .filter((card) => {
+      if (!card.description) return false;
+      card.description.tags.some((tag: Tag) => tag.localized_tag_name === "Trading Card");
+    });
 
   $: filteredCards = sortedByType
     .filter((card) => {
       if (searchQuery == "") return true;
+      if (!card.description) return false;
 
       return (
         card.description.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
         card.description.type.toLowerCase().includes(searchQuery.toLowerCase())
       );
     })
-    .sort((a, b) => a.description.type.localeCompare(b.description.type));
+    .sort((a, b) => {
+      if (!a.description || !b.description) return 0;
+
+      return a.description.type.localeCompare(b.description.type);
+    });
 </script>
 
 <svelte:head>
@@ -62,10 +61,10 @@
   <div class="flex flex-wrap justify-center gap-4">
     {#each filteredCards as card (card.classid)}
       <Card
-        iconUrl={card.description.icon_url}
+        iconUrl={card.description?.icon_url || ""}
         count={card.count}
-        name={card.description.name}
-        type={card.description.type}
+        name={card.description?.name || "Unknown Name"}
+        type={card.description?.type || "Unknown Type"}
       />
     {:else}
       No Cards
---
+++ src/routes/(trading)/inventory/[user]/steam.ts
@@ -0,0 +1,37 @@
+interface Asset {
+  appid: number;
+  contextid: string;
+  assetid: string;
+  classid: string;
+  instanceid: string;
+  amount: string;
+}
+
+interface Tag {
+  category: string;
+  internal_name: string;
+  localized_category_name: string;
+  localized_tag_name: string;
+}
+
+interface Description {
+  appid: number;
+  classid: string;
+  instanceid: string;
+  currency: number;
+  background_color: string;
+  icon_url: string;
+  icon_url_large: string;
+  descriptions: Array<{ value: string }>;
+  tradable: number;
+  name: string;
+  type: string;
+  market_name: string;
+  market_hash_name: string;
+  market_fee_app: number;
+  commodity: number;
+  market_tradable_restriction: number;
+  market_marketable_restriction: number;
+  marketable: number;
+  tags: Tag[];
+}
